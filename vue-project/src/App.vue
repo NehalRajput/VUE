f29c57455014fe2cec093677b5d0f7f812bb9fd2
NehalRajput <nehal17.ast@gmail.com>
2025-04-08 12:21:07 +0530

    second commit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,17 @@
 
 <script setup>
+import { ref } from 'vue'
+
 const name = "nehal";
 const status= 'active';
+const colors = ref(['Red', 'Green', 'Blue']);
+const link = 'https://www.google.com/';
 
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
 </script>
 
 <template>
@@ -11,5 +20,13 @@ const status= 'active';
    <p v-if="status === 'active'">user is active</p>
    <p v-else-if="status === 'pending'">user is pending</p>
    <p v-else>user is inactive</p>
+
+   <ul>
+      <li v-for="color in colors">{{ color }}</li>
+   </ul>
+   <a v-bind:href="link">click for google</a>
    
+   <h1>Count: {{ count }}</h1>
+   <button @click="increment">Increment</button>
+
 </template>
