672e93cd3451d5c6ed4af8b8ab96020f1aacdae5
NehalRajput <nehal17.ast@gmail.com>
2025-04-08 17:47:11 +0530

    vue mastery tutorial followed

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,44 +1,68 @@
-<!-----<script setup>
-import { ref, onMounted } from 'vue'
-
-const isLoading = ref(true)
-const data = ref([])
-
-onMounted(() => {
-  setTimeout(() => {
-    data.value = ['Item 1', 'Item 2']
-    isLoading.value = false
-  }, 2000) 
-})
-</script>
-
-<template>
-  <div v-if="isLoading">
-    <p>Loading...</p> 
-  </div>
-  <ul v-else>
-    <li v-for="item in data" :key="item">{{ item }}</li>
-  </ul>
-</template>
--->
-
-
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
+import Blue from './assets/images/socks_blue.jpg'
+import Green from './assets/images/socks_green.jpg'
 
-const users = ref([])
-const isLoading = ref(true)
+const product = 'Socks'
+const image = ref('./images/socks_green.jpg') 
+const inStock = true
+const cart = ref(0)
 
-onMounted(async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  users.value = await res.json()
-  isLoading.value = false
-})
+const details = ['50% cotton', '30% wool', '20% polyester']
+
+const variants = [
+  {
+    id: 2234,
+    color: 'green',
+    image: Green
+  },
+  {
+    id: 2235,
+    color: 'blue',
+    image: Blue
+  }
+]
+
+function addToCart() {
+  cart.value += 1
+}
+
+function updateImage(newImage) {
+  image.value = newImage
+}
 </script>
 
 <template>
-  <div v-if="isLoading">Loading users...</div>
-  <ul v-else>
-    <li v-for="user in users" :key="user.id">{{ user.name }}</li>
-  </ul>
+  <div>
+    <div class="nav-bar"></div>
+
+    <div class="product-display">
+      <div class="container">
+     
+        <div class="cart">Cart({{ cart }})</div>
+
+        <div class="product-image">
+          <img :src="image" alt="Product image" />
+        </div>
+
+        <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+
+          <ul>
+            <li v-for="detail in details" :key="detail">{{ detail }}</li>
+          </ul>
+
+
+          <div v-for="variant in variants" :key="variant.id" class="color-circle"
+            :style="{ backgroundColor: variant.color }" @mouseover="updateImage(variant.image)">
+          </div>
+
+
+          <button class="button" @click="addToCart">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
