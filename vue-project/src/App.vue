4074670c939b5ada59f7cab4a2ecf981305ce664
NehalRajput <nehal17.ast@gmail.com>
2025-04-08 12:18:56 +0530

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+const name = "nehal";
+const status= 'active';
+
+</script>
+
+<template>
+   
+   <h1>Hello {{ name }}</h1>
+   <p v-if="status === 'active'">user is active</p>
+   <p v-else-if="status === 'pending'">user is pending</p>
+   <p v-else>user is inactive</p>
+   
+</template>
